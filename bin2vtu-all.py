d1cddf29f3e0104b4c6682b5732aecf79afa7b4c
jrugis <j.rugis@auckland.ac.nz>
2018-03-01 20:54:27 +1300

    basal

--- bin2vtu-all.py
+++ bin2vtu-all.py
@@ -11,13 +11,11 @@ from evtk.vtk import VtkTriangle
 
 def read_bin(fname):
   f1 = open(fname + '.bin', 'rb') # open the binary file
-
   # get the vertices
   nverts = struct.unpack('i', f1.read(4))[0]
   verts = np.empty([nverts, 3])
   for i in range(nverts):
     verts[i] = struct.unpack('fff', f1.read(12))
-
   # get the tris
   ntris = struct.unpack('i', f1.read(4))[0]
   tris = np.empty([ntris, 3], dtype=int)
@@ -25,7 +23,6 @@ def read_bin(fname):
   for i in range(ntris):
     tris[i] = struct.unpack('iii', f1.read(12))
     dnl[i] = struct.unpack('f', f1.read(4))[0]
-
   f1.close # close the binary file 
   return verts, tris, dnl
 
@@ -36,168 +33,130 @@ def write_points(fname, verts):
   for i in range(nverts):
     for j in range(3):
       xyz[j,i] = verts[i,j]  
-  pointsToVTK("nodes_"+fname, \
+  pointsToVTK(fname, \
     xyz[0,:], xyz[1,:], xyz[2,:], \
     data=None)  # write out vtu file
   return
 
 ##################################################################
-def write_tris(fname, verts, tris, dnl):
-
+def write_tris(fname, verts, tris, data=None):
   nverts = verts.shape[0]
   xyz = np.empty([3, nverts]) # because it needs re-ordering
   for i in range(nverts):
     for j in range(3):
       xyz[j,i] = verts[i,j]  
-
   ntris = tris.shape[0]
   conn = np.empty(3*ntris)
   for i in range(ntris):
     conn[3*i] = tris[i,0]-1 # index from zero
     conn[3*i+1] = tris[i,1]-1
     conn[3*i+2] = tris[i,2]-1
-
   offset = np.zeros(ntris, dtype=int)
   for i in range(ntris):
     offset[i] = 3*(i+1) 
-
   ctype = np.zeros(ntris)
   for i in range(ntris):
     ctype[i] = VtkTriangle.tid 
-
-  unstructuredGridToVTK("surface_"+fname, \
+  unstructuredGridToVTK(fname, \
     xyz[0,:], xyz[1,:], xyz[2,:], \
     connectivity=conn, offsets=offset, cell_types=ctype, \
-    cellData= {"dnl" : dnl}, pointData=None)  # write out vtu file
-
+    cellData=data, pointData=None)  # write out vtu file
   return
 
 ##################################################################
-def write_apical(fname, verts, tris, dnl, radius):
-
-  conn = np.empty(3*tris.shape[0]) # more than needed...
-  c = 0
-  for i, t in enumerate(tris):
-    if dnl[i] < radius:
-      conn[c] = t[0]-1; c+=1 # index from zero
-      conn[c] = t[1]-1; c+=1
-      conn[c] = t[2]-1; c+=1
-  conn = conn[0:c]  # trim the array to actually used
-
-  ntris = c/3
-  offset = np.zeros(ntris, dtype=int)
-  for i in range(ntris):
-    offset[i] = 3*(i+1) 
-
-  ctype = np.zeros(ntris)
-  for i in range(ntris):
-    ctype[i] = VtkTriangle.tid 
-
-  nverts = verts.shape[0]
-  xyz = np.empty([3, nverts])
-  for i in range(nverts): # all verts (not just the apical)
-    for j in range(3):
-      xyz[j,i] = verts[i,j]  
-
-  unstructuredGridToVTK("apical_"+fname, \
-    xyz[0,:], xyz[1,:], xyz[2,:], \
-    connectivity=conn, offsets=offset, cell_types=ctype, \
-    cellData=None, pointData=None)  # write out vtu file
-
-  return
+def get_apical(tris, dnl, small_radius, large_radius):
+  atrisS = []
+  atrisL = []
+  for i, d in enumerate(dnl):
+    if d < small_radius: atrisS.append(i+1)
+    if d < large_radius: atrisL.append(i+1)
+  return(np.array(atrisS, dtype=int), np.array(atrisL, dtype=int))
 
 ##################################################################
-def tris_in_verts(vi, tris): # vertex indices, tris
+def get_basal(tris, ctrisi, atrisi):
+  btrisi = np.array([range(tris.shape[0])], dtype=int) + 1 # all indices
+  btrisi = np.setdiff1d(btrisi, ctrisi) # remove common
+  btrisi = np.setdiff1d(btrisi, atrisi) # remove apical Large
+  return(btrisi)
+
+##################################################################
+def find_tris(vi, tris): # vertex indices, tris
   ti = []
-  for t in tris: # find tris associated with verticies in list
-    ans = np.where(vi == t[0])
+  for i, t in enumerate(tris): # find tris associated with verticies in list
+    ans = np.where(vi == t[0]) # first vertex in list?
     if len(ans[0]) == 0: continue
-    ans = np.where(vi == t[1])
+    ans = np.where(vi == t[1]) # second vertex in list?
     if len(ans[0]) == 0: continue
-    ans = np.where(vi == t[2])
+    ans = np.where(vi == t[2]) # third vertex in list?
     if len(ans[0]) == 0: continue
-    ti.append(t)
-  return np.array(ti, dtype=int)
+    ti.append(i+1)
+  return np.array(ti, dtype=int) # return tri indicies
 
 ##################################################################
-def common_tris(lvertsA, trisA, lvertsB):
+def get_common(vertsA, trisA, vertsB):
   cviA = []
-  for v in lvertsB: # first find common vertices 
-    ans = np.where(np.linalg.norm(lvertsA-v,axis=1) < 0.001)
+  for v in vertsB: # first find common vertices 
+    ans = np.where(np.linalg.norm(vertsA-v,axis=1) < 0.001)
     if len(ans[0]) != 0:
       if len(ans[0]) > 1:
         print("ERROR: duplicate vertices")
       cviA.append(ans[0][0]+1)
-
   cviA = np.array(cviA, dtype=int)
-  #ctiA = []
-  #for t in trisA:
-  #  ans = np.where(cviA == t[0])
-  #  if len(ans[0]) == 0: continue
-  #  ans = np.where(cviA == t[1])
-  #  if len(ans[0]) == 0: continue
-  #  ans = np.where(cviA == t[2])
-  #  if len(ans[0]) == 0: continue
-  #  ctiA.append(t)
-  #return np.array(ctiA, dtype=int)
-  return(tris_in_verts(cviA,trisA))
-
-##################################################################
-def write_common(fname, verts, tris):
-  nverts = verts.shape[0]
-  xyz = np.empty([3, nverts]) # because it needs re-ordering
-  for i in range(nverts):
-    for j in range(3):
-      xyz[j,i] = verts[i,j]  
-
-  ntris = tris.shape[0]
-  conn = np.empty(3*ntris)
-  for i in range(ntris):
-    conn[3*i] = tris[i,0]-1 # index from zero
-    conn[3*i+1] = tris[i,1]-1
-    conn[3*i+2] = tris[i,2]-1
-
-  offset = np.zeros(ntris, dtype=int)
-  for i in range(ntris):
-    offset[i] = 3*(i+1) 
-
-  ctype = np.zeros(ntris)
-  for i in range(ntris):
-    ctype[i] = VtkTriangle.tid 
-
-  unstructuredGridToVTK("common_"+fname, \
-    xyz[0,:], xyz[1,:], xyz[2,:], \
-    connectivity=conn, offsets=offset, cell_types=ctype, \
-    cellData=None, pointData=None)  # write out vtu file
-
-  return
+  return(find_tris(cviA,trisA)) # return tri indicies
 
 ##################################################################
 # main program
 ##################################################################
 
-lverts = []  # vertices for each cell
-ltris = []       # tris for each cell
+# lists of data for each cell 
+#  NOTE: one-based indexing
+lverts   = []  # vertices
+ltris    = []  # tris
+latrisiS = []  # apical tri indices Small
+latrisiL = []  # apical tri indices Large
+lctrisi  = []  # common tri indices
+lbtrisi  = []  # basal tri indices
 
+print("individual cell calcs")
 mesh_names = subprocess.check_output("ls *tet.bin", shell=True).split()
 for mesh in mesh_names:
   fname = mesh.split('.')[0]
-  print fname
+  print fname[-4]
   verts, tris, dnl = read_bin(fname)
+  atrisiS, atrisiL = get_apical(tris, dnl, 0.8, 1.4) # apical distances
+  write_points("nodes_"+fname, verts)
+  write_tris("surface_"+fname, verts, tris, data={"dnl" : dnl})
+  write_tris("apical_"+fname, verts, tris[atrisiS-1]) # has all the verts
   lverts.append(verts)
   ltris.append(tris)
-  #write_points(fname, verts)
-  #write_tris(fname, verts, tris, dnl)
-  #write_apical(fname, verts, tris, dnl, 0.8)
+  latrisiS.append(atrisiS)
+  latrisiL.append(atrisiL)
 
+print("cell-to-cell calcs")
 ncells = len(mesh_names)
 for c1 in range(ncells):
-  for c2 in range(c1+1, ncells):
-    ctris = common_tris(lverts[c1], ltris[c1], lverts[c2])
-    if ctris.shape[0] != 0:
-      fname = str(c1+1) + str(c2+1)
-      print fname
-      write_common(fname, lverts[c1], ctris)
+  ctrisi = np.array([], dtype=int) # common tri indices per cell
+  for c2 in range(ncells):
+    if(c1==c2): continue
+    print c1+1, c2+1
+    cti = get_common(lverts[c1], ltris[c1], lverts[c2]) # pair-wise common
+    if cti.shape[0] != 0:
+      ctrisi = np.concatenate([ctrisi,cti])
+      if(c2 > c1):
+        fname = "common_" + str(c1+1) + "c" + str(c2+1) + "c"
+        write_tris(fname, lverts[c1], ltris[c1][cti-1]) # has all the verts
+  lctrisi.append(ctrisi)
+
+print("additional cell calcs")
+for i, mesh in enumerate(mesh_names):
+  print str(i+1)
+  fname = mesh.split('.')[0]
+  btrisi = get_basal(ltris[i], lctrisi[i], latrisiL[i])
+  write_tris("basal_"+fname, lverts[i], ltris[i][btrisi-1]) # has all the verts
+  lbtrisi.append(btrisi)
+
 print "DONE."
 
+##################################################################
+##################################################################
 
